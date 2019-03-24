7732f7918cb6f45ec538d8569a4ba108ecf836b8
jay suthar <40814722+jaysuthar743@users.noreply.github.com>
2019-03-24 19:03:02 +0530

    Update Download_video.py

--- Download_video.py
+++ Download_video.py
@@ -1,5 +1,6 @@
 import pytube
-video_url = 'https://www.youtube.com/watch?v=tXOIvjbNhts' // paste here your Youube videos' url
+video_url = 'https://www.youtube.com/watch?v=tXOIvjbNhts' # copy and paste url
 youtube = pytube.YouTube(video_url)
 video = youtube.streams.first()
-video.download('/home/jay/Downloads') // path, where to video download.
+video.download('/home/jay/Downloads') # path, where to video download.
+# it may take some tome to download.
